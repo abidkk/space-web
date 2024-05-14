d169d475837493050c60bb48111ec4349b988da8
abidkk <dev.abidkk@gmail.com>
2024-05-14 17:32:42 +0500

    membership section

--- app.js
+++ app.js
@@ -2,24 +2,20 @@ $(document).ready(function () {
   $(".membership-btn-team").click(function () {
     // $(".membership-tool--body").toggle();
     $(".membership-tool--body").show(); // Shows
-    $(".membership-btn-remote").removeClass("active-tab");
-    $(".membership-btn-solo").removeClass("active-tab");
+    $(".membership-btn-remote,.membership-btn-solo").removeClass("active-tab");
 
     $(this).addClass("active-tab");
   });
 
   $(".membership-btn-solo").click(function () {
-    $(".membership-btn-team").removeClass("active-tab");
-    $(".membership-btn-remote").removeClass("active-tab");
+    $(".membership-btn-team,.membership-btn-remote").removeClass("active-tab");
     $(this).addClass("active-tab");
     $(".membership-tool--body").hide(); // hide 
   });
 
   $(".membership-btn-remote").click(function () {
-    $(".membership-btn-team").removeClass("active-tab");
-    $(".membership-btn-solo").removeClass("active-tab");
+    $(".membership-btn-team,.membership-btn-solo").removeClass("active-tab");
     $(".membership-tool--body").hide(); // hide 
-
     $(this).addClass("active-tab");
   });
 });
